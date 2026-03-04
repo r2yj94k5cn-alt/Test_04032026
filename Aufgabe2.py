19d89cb6d7cb970426820cbe6ac1427c5cb5848f
Florin Keller <florin.keller@hispeed.ch>
2026-03-04 10:59:25 +0100

    Änderung auf human_feature Branch

--- Aufgabe2.py
+++ Aufgabe2.py
@@ -5,3 +5,11 @@ def addiere(a, b):
 print("Hallo Welt!")
 ergebnis = addiere(3, 4)
 print("3 + 4 =", ergebnis)
+
+
+def subtrahiere(a, b):
+    return a - b
+
+
+ergebnis_subtraktion = subtrahiere(10, 5)
+print("10 - 5 =", ergebnis_subtraktion)
